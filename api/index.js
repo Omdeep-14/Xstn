97f7bfc6b6301c1d597ee2a7c60234d805508383
Omdeep-14 <l>
2026-03-05 02:37:38 +0530

    deploment changes

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 try {
   app.listen(PORT, () => {
-    console.log(`Server started on PORT ${PORT}`);
+    console.log(`Server started  PORT ${PORT}`);
   });
 } catch (error) {
   console.error("Problem starting server", error);
